lervag/wiki.vim

commit d9004aed7d7cb50283f64a4dff8515219043c11c

feat: apply g:wiki_link_target_map to WikiOpen

autoload/wiki/page.vim
@@ -5,9 +5,18 @@
 " License:    MIT license
 "
 
-function! wiki#page#open() abort "{{{1
-  let l:page = input('Open/Create page: ')
+function! wiki#page#open(page) abort "{{{1
+  let l:page =
+        \ !empty(g:wiki_link_target_map) && exists('*' . g:wiki_link_target_map)
+        \ ? call(g:wiki_link_target_map, [a:page])
+        \ : ''
   call wiki#url#parse('wiki:/' . l:page).open()
+endfunction
+
+"}}}1
+function! wiki#page#open_ask() abort "{{{1
+  let l:page = input('Open/Create page: ')
+  call wiki#page#open(l:page)
 endfunction
 
 "}}}1

plugin/wiki.vim
@@ -50,7 +50,7 @@ call wiki#init#option('wiki_zotero_root', '~/.local/zotero')
 " Initialize global commands
 command! WikiEnable   call wiki#buffer#init()
 command! WikiIndex    call wiki#goto_index()
-command! WikiOpen     call wiki#page#open()
+command! WikiOpen     call wiki#page#open_ask()
 command! WikiReload   call wiki#reload()
 command! WikiJournal  call wiki#journal#make_note()
 command! CtrlPWiki    call ctrlp#init(ctrlp#wiki#id())

commit ef0377f157044ba5bbe47d1598565ac428afc8c1

Fix bug introduced in d9004ae
d9004ae makes WikiOpen fail to open or create a page if no
wiki_link_target_map is set.

autoload/wiki/page.vim
@@ -9,7 +9,7 @@ function! wiki#page#open(page) abort "{{{1
   let l:page =
         \ !empty(g:wiki_link_target_map) && exists('*' . g:wiki_link_target_map)
         \ ? call(g:wiki_link_target_map, [a:page])
-        \ : ''
+        \ : a:page
   call wiki#url#parse('wiki:/' . l:page).open()
 endfunction
 