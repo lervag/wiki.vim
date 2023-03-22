e5fc41b5eebb4d5d704ff61d38d5ed7a2e809985
saccarosium <github.e41mv@aleeas.com>
2023-03-22 12:53:39 +0100

    fix: path relative to g:wiki_path

--- lua/wiki/init.lua
+++ lua/wiki/init.lua
@@ -1,14 +1,28 @@
 local M = {}
 
+-- Returns shorten path
+local function filter(path)
+    local root = vim.g.wiki_root .. "/"
+    return path:gsub(root, "")
+end
+
+-- Returns full path
+local function unfilter(path)
+    local root = vim.g.wiki_root .. "/"
+    return root .. path
+end
+
 function M.get_pages()
     local res = {}
     local pages = vim.fn["wiki#page#get_all"]()
     for _, p in pairs(pages) do
-        table.insert(res, p[1])
+        local page = filter(p[1])
+        table.insert(res, page)
     end
     vim.ui.select(res,
         { prompt = "WikiPages> " },
         function(f)
+            f = unfilter(f)
             vim.cmd("edit " .. f)
         end)
 end
@@ -18,14 +32,14 @@ function M.get_tags()
     local tags = vim.fn["wiki#tags#get_all"]()
     for key, val in pairs(tags) do
         for _, file in pairs(val) do
-            local str = string.format("%s: %s:%s", key, file[1], file[2])
+            local str = string.format("%s:%s:%s", key, file[2], filter(file[1]))
             table.insert(res, str)
         end
     end
     vim.ui.select(res,
         { prompt = "WikiTags> " },
         function(t)
-            t = vim.split(t, ':')[2]
+            t = unfilter(vim.split(t, ':')[3])
             vim.cmd("edit " .. t)
         end)
     return res
@@ -36,13 +50,13 @@ function M.toc()
     local toc = vim.fn["wiki#toc#gather_entries"]()
     for _, hd in pairs(toc) do
         local indent = vim.fn["repeat"]('.', hd.level - 1)
-        local line = hd.lnum .. ': ' .. indent .. hd.header
+        local line = hd.lnum .. '|' .. indent .. hd.header
         table.insert(res, line)
     end
     vim.ui.select(res,
         { prompt = "WikiToc> ", },
         function(t)
-            t = vim.split(t, ':')[1]
+            t = vim.split(t, '|')[1]
             vim.cmd("execute " .. t)
         end)
 end
